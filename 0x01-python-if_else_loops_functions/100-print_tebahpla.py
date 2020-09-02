c36479719a0c09c716b749ee8dd752cc13515c88
lperezcas16 <lperezcas@unbosque.edu.co>
2020-09-01 22:39:39 -0500

    [update] 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
     if i % 2 == 0:
-        print("{0}".format(chr(i)), end='')
+        print("{:c}".format(i), end='')
     else:
-        print("{0}".format(chr(i - 32)), end='')
+        print("{:c}".format(i - 32), end='')
