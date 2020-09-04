5d4e23999e4d3632a75460bc4c1964beb12cde7f
lperezcas16 <lperezcas@unbosque.edu.co>
2020-09-03 20:21:03 -0500

    [update] 102

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+import dis
+
+
+def sum(a, b):
+
+    from magic_calculation_102 import add, sub
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+    else:
+        return (sub(a, b))
+
+
+dis.dis(sum)
