2111834b6de6860f7ecf399081054d43ee60b506
lperezcas16 <lperezcas@unbosque.edu.co>
2020-09-08 22:26:07 -0500

    [fix] #9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if  my_list is None or len(my_list) <= 0:
+    if  len(my_list) == 0:
         return None
     my_list.sort()
     return my_list[-1]
---
+
