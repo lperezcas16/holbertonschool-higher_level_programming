dd8df5bcbd5501d25a969958856069bb35cedc53
lperezcas16 <lperezcas@unbosque.edu.co>
2020-09-09 16:26:10 -0500

    [update] #13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome -  checks if a singly linked list is a palindrome
+ * @head: pointer to head of list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *temp = NULL;
+	int buff[3000], len = 0;
+
+	if (head == NULL || *head == NULL || (*head)->next == NULL)
+	{
+		return (1);
+	}
+	temp = *head;
+
+	for (len = 0; temp; len++)
+	{
+		buff[len] = temp->n;
+		temp = temp->next;
+	}
+	len--;
+	temp = *head;
+	for (; temp; len--)
+	{
+		if (buff[len] != temp->n)
+		{
+			return (0);
+		}
+		temp = temp->next;
+	}
+
+	return (1);
+}
