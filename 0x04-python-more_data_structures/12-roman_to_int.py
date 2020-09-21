f8552ca555609a17a20a9715b8752317c2489f64
lperezcas16 <lperezcas@unbosque.edu.co>
2020-09-21 17:58:59 -0500

    [update]

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    num = 0
-    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
-    if type(roman_string) is not str:
+    my_dicc = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = 0
+
+    if roman_string is None or type(roman_string) is not str:
         return 0
 
     for i in range(len(roman_string)):
         if i == len(roman_string) - 1:
-            num += d[roman_string[i]]
-        elif d[roman_string[i]] >= d[roman_string[i + 1]]:
-            num += d[roman_string[i]]
+            num += my_dicc[roman_string[i]]
+        elif my_dicc[roman_string[i]] >= my_dicc[roman_string[i + 1]]:
+            num += my_dicc[roman_string[i]]
         else:
-            num -= d[roman_string[i]]
-
+            num -= my_dicc[roman_string[i]]
     return num
+
+#
+# X = 10
+# VII = 7
+# IX = 9
+# LXXXVII = 87
+# DCCVII = 707
