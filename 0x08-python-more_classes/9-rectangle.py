385c62fc261ef6e47b687a0a174e87867d4c7a03
lperezcas16 <lperezcas@unbosque.edu.co>
2020-09-28 19:54:43 -0500

    [fix] #9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -53,17 +53,17 @@ class Rectangle:
 
     def __str__(self):
         rect = ""
-        if self.__width == 0 or self.__height == 0:
+        if self.width == 0 or self.height == 0:
             return rect
-        for i in range(self.__height):
-            for j in range(self.__width):
+        for i in range(self.height):
+            for j in range(self.width):
                 rect += str(self.print_symbol)
-            if (i < self.__height - 1):
+            if (i != self.height - 1):
                 rect += '\n'
         return rect
 
     def __repr__(self):
-        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+        return "Rectangle({:d}, {:d})".format(self.width, self.height)
 
     def __del__(self):
         Rectangle.number_of_instances -= 1
