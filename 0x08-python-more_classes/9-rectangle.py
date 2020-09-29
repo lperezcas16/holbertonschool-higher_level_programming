a0dbd65f015a75ebfa1e8c9470e1073db8e12519
lperezcas16 <lperezcas@unbosque.edu.co>
2020-09-28 19:47:43 -0500

    [fix] public variable #9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -42,12 +42,12 @@ class Rectangle:
     """Functions"""
 
     def area(self):
-        return self.__width * self.__height
+        return self.width * self.height
 
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.__width + self.__height)
+        return 2 * (self.width + self.height)
 
     """toSring"""
 
