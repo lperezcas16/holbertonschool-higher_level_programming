61500a87f6eda4fa6c530225c9b38e46163898c5
lperezcas16 <lperezcas@unbosque.edu.co>
2020-10-02 20:06:07 -0500

    [update] files

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Function to create hash tables
+ * @size: the size of the hash table
+ *
+ * Return: pointer to the hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *head = NULL;
+
+	head = malloc(sizeof(hash_table_t));
+
+	if (!head)
+		return (NULL);
+	head->array = calloc(size, sizeof(hash_node_t));
+	if (!head->array)
+		return (NULL);
+	head->size = size;
+
+	return (head);
+}
