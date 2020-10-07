8e71d1f2ef6cc1928d7f9b0d7e7323c17263babe
lperezcas16 <lperezcas@unbosque.edu.co>
2020-10-06 23:19:09 -0500

    [update] #100 #101

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" Class MyInit-->inherits from Int """
+
+
+class MyInt(int):
+    """ Class MyInit--> inherits from Int """
+    def __init__(self, value):
+        self.value = value
+    """ equal to different """
+    def __eq__(self, other):
+        return self.value != other
+    """ different to equal"""
+    def __ne__(self, other):
+        return self.value == other
--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" Adds a new attribute to an object """
+
+
+def add_attribute(obj, name, value):
+    """ Adds a new attribute to an object """
+    if hasattr(obj, '__dict__') is False:
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, name, value)
