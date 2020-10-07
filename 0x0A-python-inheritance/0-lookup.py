0c87b10bdec2ceb798a2ec50928ecdc796cefb61
lperezcas16 <lperezcas@unbosque.edu.co>
2020-10-06 19:31:48 -0500

    [update] files

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" return available attributes and methods of an object:"""
+
+
+def lookup(obj):
+    """ Function"""
+    return dir(obj)
--- 0x1A-hash_tables/4-hash_table_get.c
+++
@@ -1,9 +0,0 @@
-#include "hash_tables.h"
-/**
- * hash_table_get -
- *
- * Return: 
- */
-char *hash_table_get(const hash_table_t *ht, const char *key)
-{
-}
