8a61d2398a5f4aed34445dec59f3eb20096fcddd
lperezcas16 <lperezcas@unbosque.edu.co>
2020-10-06 21:15:17 -0500

    [update] #4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+"""the object is an instance of a class that inherited"""
+
+
 def inherits_from(obj, a_class):
+    """Function"""
+    if type(obj) != a_class:
+        return isinstance(obj, a_class)
+    return False
