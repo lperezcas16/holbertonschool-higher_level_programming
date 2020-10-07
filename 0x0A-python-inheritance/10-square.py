0bb3e36a5a0538cd65cf63a6fe2366603a3e3e0b
lperezcas16 <lperezcas@unbosque.edu.co>
2020-10-06 22:55:14 -0500

    [update] #10 #11#

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Class"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Class Square"""
+    def __init__(self, size):
+        if self.integer_validator("size", size):
+            self.__size = size
+        super().__init__(size, size)
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Class"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """class square"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Square size"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
