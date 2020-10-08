20a9f3f3dcf65cf136805e4b8977a3e938c80083
lperezcas16 <lperezcas@unbosque.edu.co>
2020-10-08 18:40:13 -0500

    [update] #12

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Class student"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Return class dictionary """
+        if type(attrs) == list and all(type(items) == str for items in attrs):
+            dict = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    dict[i] = self.__dict__[i]
+            return dict
+        return self.__dict__
