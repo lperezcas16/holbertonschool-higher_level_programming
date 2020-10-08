692bca8faf9e1ad078e5bdd9cfb33f35ebd1aaf8
lperezcas16 <lperezcas@unbosque.edu.co>
2020-10-08 17:24:39 -0500

    [update] #7

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+"""OBJ -> txt"""
+import json
+
+
 def save_to_json_file(my_obj, filename):
+    """ method OBJ -> txt"""
+    with open(filename, 'w+') as fil:
+        json.dump(my_obj, fil)
