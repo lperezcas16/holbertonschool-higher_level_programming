ace16c34b6108fbca4c9bdacb5a13a9084dc49f1
lperezcas16 <lperezcas@unbosque.edu.co>
2020-10-13 21:00:12 -0500

    [add-delete]  commits rectangle, base_geometry

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,7 +82,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("width must be >0")
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @height.setter
@@ -91,7 +91,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError("height must be >0")
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @x.setter
@@ -107,7 +107,7 @@ class Rectangle(Base):
     def y(self, value):
         """Setter"""
         if type(value) is not int:
-            raise TypeError("x must be an integer")
+            raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("x must be >= 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
