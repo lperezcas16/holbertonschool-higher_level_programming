1a0744dad307a5e11b47599819fa89646b2e9820
lperezcas16 <lperezcas@unbosque.edu.co>
2020-10-13 20:35:51 -0500

    [fix] rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """ Rectancle project """
 from models.base import Base
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
-class Rectangle(Base, BaseGeometry):
+class Rectangle(Base):
     """Class Rectangle """
 
     def __init__(self, width, height, x=0, y=0, id=None):
@@ -74,20 +73,32 @@ class Rectangle(Base, BaseGeometry):
     """Setters"""
     @width.setter
     def width(self, value):
-        self.integer_validator("width", value)
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be >0")
         self.__width = value
 
     @height.setter
     def height(self, value):
-        self.integer_validator("height", value)
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be >0")
         self.__height = value
 
     @x.setter
     def x(self, value):
-        self.integer_validator2("x", value)
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @y.setter
     def y(self, value):
-        self.integer_validator2("y", value)
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__y = value
