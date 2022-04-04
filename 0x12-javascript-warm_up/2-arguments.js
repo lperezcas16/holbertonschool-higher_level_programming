9c10eb9364a277b5eee30d9bbd4f2d328785fb72
lperezcas16 <lperezcas@unbosque.edu.co>
2022-04-04 15:16:19 -0500

    fix file #2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,6 +3,8 @@ const args = process.argv;
 
 if (args.length <= 2) {
   console.log('No argument');
+} else if (args.length == 3){
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
