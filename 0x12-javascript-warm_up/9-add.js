f59fd72a0e744d32b219940c7113483af8015ae2
lperezcas16 <lperezcas@unbosque.edu.co>
2022-04-04 19:57:42 -0500

    fix file #9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 const myVar = process.argv;
 
 if (parseInt(myVar[2]) && parseInt(myVar[3])) {
-	console.log(parseInt(myVar[3]) + parseInt(myVar[2]));
+  console.log(parseInt(myVar[3]) + parseInt(myVar[2]));
 } else {
-	console.log('NaN');
+  console.log('NaN');
 }
