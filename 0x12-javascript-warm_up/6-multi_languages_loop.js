8cdf9cc7803c889da5b319cd6c46763f1c2adbce
lperezcas16 <lperezcas@unbosque.edu.co>
2022-04-04 19:05:52 -0500

    add file #6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let iterator = 0; iterator < myArray.length; iterator++) {
+	console.log(myArray[iterator])
+}
