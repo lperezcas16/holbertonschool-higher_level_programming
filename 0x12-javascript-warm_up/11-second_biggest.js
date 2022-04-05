2b71130a13282c3839edb60157e4a266cbdbdedc
lperezcas16 <lperezcas@unbosque.edu.co>
2022-04-05 13:20:17 -0500

    add file #11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ const myVar = process.argv;
 
 if (parseInt(myVar[2] && parseInt(myVar[3]))) {
   myVar.sort(function (a, b) { return b - a; });
-  const highest = myVar[2];
+  const highest = myVar[3];
   console.log(highest);
 } else {
   console.log('0');
