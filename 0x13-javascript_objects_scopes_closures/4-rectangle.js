4b02096a750cbeb2b71223f9ee5619c363f436e5
lperezcas16 <lperezcas@unbosque.edu.co>
2022-04-07 11:45:13 -0500

    add file #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/osr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print(){
+    let i = 0;
+    for (i; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+  rotate (){
+    const cont = this.height;
+	  this.width = this.height;
+	  this.height = cont;
+    }
+  double(){
+    this.width = this.width * 2
+    this.height = this.height * 2
+  }
+    
+}
+module.exports = Rectangle;
