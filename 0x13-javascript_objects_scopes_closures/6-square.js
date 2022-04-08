fc22a32c62b70924a627912bea0d5ddcaa9c266d
lperezcas16 <lperezcas@unbosque.edu.co>
2022-04-07 19:58:07 -0500

    add file #6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const oldSquare = require("./5-square.js");
+class Square extends oldSquare{
+  charPrint (c) {
+       if(!c){
+         printSquare('X', this.width, this.height);
+       } else {
+         printSquare('C', this.width, this.height);
+       }
+  }
+}
+
+  function printSquare(character, width, height) {
+    for (let i = 0; i < height; i++){
+      console.log(character.repeat(width));
+    }
+  }
+module.exports = Square;
