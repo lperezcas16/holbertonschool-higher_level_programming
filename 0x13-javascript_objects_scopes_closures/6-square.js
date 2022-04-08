508b2327331a9781dd79085241d9ca80d9a6f5e1
lperezcas16 <lperezcas@unbosque.edu.co>
2022-04-07 20:44:58 -0500

    fix edge cases file #6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,11 +4,11 @@ const OldSquare = require('./5-square.js');
 class Square extends OldSquare {
   charPrint (c) {
     let character = 'X';
-    if (c !== undefined){
-      character = c
+    if (c !== undefined) {
+      character = c;
     }
-    for (let i = 0; i < this.height; i++){
-      console.log(character.repeat(this.width))
+    for (let i = 0; i < this.height; i++) {
+      console.log(character.repeat(this.width));
     }
   }
 }
