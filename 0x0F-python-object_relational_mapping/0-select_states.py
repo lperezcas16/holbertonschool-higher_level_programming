57861c2e5b9d5d87d5df12de9dd6a7f8395a91fa
lperezcas16 <lperezcas@unbosque.edu.co>
2022-05-11 19:33:40 -0500

    [ADD] file #2

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" lists all states from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    argv = sys.argv
+
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+
+    """ connection to a db """
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database_name,
+        port=3306
+    )
+
+    """ Nos da la capacidad de tener múltiples entornos de trabajo """
+    cur = db.cursor()
+
+    """ execute to execte a query """
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    """ tuple wiat all rows """
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
